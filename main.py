38e5fdb74d4883ba647d42b91e82c3979ef07b51
Aleksandr Petrov <felispimeja@gmail.com>
2023-01-02 20:02:10 +0300

    Initiate parsing position details

--- main.py
+++ main.py
@@ -6,12 +6,12 @@
 # from selenium.common.exceptions import StaleElementReferenceException
 # from selenium.webdriver.support import expected_conditions
 # from selenium.webdriver.support.wait import WebDriverWait
-# import pandas as pd
 # # import click
 # import time
 
 import itertools
-import bs4, requests
+import bs4, re, requests
+import pandas as pd
 
 from sqlalchemy import create_engine
 from sqlalchemy import types
@@ -28,180 +28,112 @@ with open('./.env') as f:
         env_vars[key] = value # Save to a dict, initialized env_vars = {}
 
 #  Generate target urls:
-base_url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search'
+base_url = 'https://www.linkedin.com/jobs/api/seeMoreJobPostings/search'
 key_words = ['gis', 'geo', 'postgis', 'fme', 'qgis', 'arcgis', 'geospatial']
 localions = ['Netherlands', 'Belgium', 'Norway', 'Sweden', 'Denmark', 'Germany', 'Ireland']
 
-url_list = [f'{base_url}?keywords={word}&location={location}&start=0'
+url_list = [f'{base_url}?keywords={word}&location={location}&start='
             for word, location in itertools.product(key_words, localions)]
 
 # https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=geo&location=Netherlands&start=600
 
 # for idx, url in enumerate(url_list): print(idx, url)
-url = url_list[14]
+url = url_list[14] #21
 
 proxy = 'socks5://localhost:9051'
-proxies = {
-              "http"  : proxy,
-              "https" : proxy,
-              "ftp"   : proxy
-            }
+proxies = {"http": proxy, "https": proxy, "ftp": proxy }
 
-page = requests.get(url=url, proxies=proxies)
-soup = bs4.BeautifulSoup(page.text, 'html.parser')
-lis = soup.find_all('li')
+job_list = []
+df = pd.DataFrame(columns=[
+    'id', 'date', 'title', 'company', 'company_link', 'location', 'link',
+    'description', 'seniority', 'employment_type', 'job_function', 'industries'])
+start = 0
 
-# print(url)
-for idx, li in enumerate(lis):
-    job_id = li.select_one('.base-search-card--link')['data-entity-urn'].split(sep=':')[-1]
-    job_title = li.select_one('h3.base-search-card__title').get_text().strip()
-    company_name = li.select_one('h4').get_text().strip()
-    location = li.select_one('.job-search-card__location').get_text().strip()
-    date = li.select_one('time')['datetime']
-    if li.select_one('a.base-card__full-link'):
-        job_link = li.select_one('a.base-card__full-link')['href']
-    else:
-        job_link = li.select_one('a.base-search-card--link')['href']
-    if li.find('a', class_='hidden-nested-link'):
-        company_link = li.select_one('a.hidden-nested-link')['href']
-    else:
-        company_link = None
-    print(f'{idx+1: 3d}', job_id, date, job_title[0:30], company_name[0:10], location, job_link, company_link)
+while start > -1:
+    # print(url + str(start))
+    page = requests.get(url=url + str(start), proxies=proxies)
+    soup = bs4.BeautifulSoup(page.text, 'html.parser')
+    if soup.find('li') is None:
+        break
+
+    start = start+25
+    lis = soup.find_all('li')
+
+    for idx, li in enumerate(lis):
+        job_id = li.select_one('.base-search-card--link')['data-entity-urn'].split(sep=':')[-1]
+        date = li.select_one('time')['datetime']
+        job_title = li.select_one('h3.base-search-card__title').get_text().strip()
+        company_name = li.select_one('h4').get_text().strip()
+        if li.select_one('a.hidden-nested-link'):
+            company_link = li.select_one('a.hidden-nested-link')['href'].split(sep='?')[0]
+        else:
+            company_link = None
+        company_link = company_link
+        location = li.select_one('.job-search-card__location').get_text().strip()
+        if li.select_one('a.base-card__full-link'):
+            job_link = li.select_one('a.base-card__full-link')['href']
+        else:
+            job_link = li.select_one('a.base-search-card--link')['href']
+        job_link = job_link.split(sep='?')[0].replace('nl.linkedin.com', 'linkedin.com')
+        # print(f'{idx+1: 3d}', job_id, date, job_title[0:30], company_name[0:10], location, job_link, company_link)
+
+        job_details = requests.get(url=job_link, proxies=proxies)
+        content = bs4.BeautifulSoup(job_details.text, 'html.parser').select_one('div.decorated-job-posting__details')
+        description = content.select_one('div.description__text--rich').get_text('\n', strip=True)
+        further_details = content.select('li.description__job-criteria-item')
+        seniority = None
+        employment_type = None
+        job_function = None
+        industries = None
+        for detail in further_details:
+            detail_type = detail.select_one('h3').get_text().strip()
+            detail_detail = detail.select_one('span').get_text().strip()
+            if detail_type == 'Seniority level':
+                seniority = detail_detail
+            elif detail_type == 'Employment type':
+                employment_type = detail_detail
+            elif detail_type == 'Job function':
+                job_function = detail_detail.replace('and', '').replace(', ,', ',').split(sep=', ')
+            elif detail_type == 'Industries':
+                industries = detail_detail.replace('and', '').replace(', ,', ',').split(sep=', ')
+        # print(seniority, employment_type, job_function, industries)
+
+        info_string = (job_id, date, job_title, company_name, location, job_link, company_link,
+                       description, seniority, employment_type, job_function, industries)
+        print(f'{idx+1: 3d}', info_string)
+        job_list.append(info_string)
+        df = pd.concat([pd.DataFrame(
+            [[job_id, date, job_title, company_name, company_link, location, job_link,
+              description, seniority, employment_type, job_function, industries]], columns=df.columns
+        ), df], ignore_index=True)
+
+# print(job_list)
+print(df.to_string())
+
+# Write data into Postgres
+connection = create_engine(f'postgresql://{env_vars["user"]}:{env_vars["password"]}@{env_vars["host"]}:'
+                           f'{env_vars["port"]}/{env_vars["dbname"]}')
+
+df.to_sql('open_positions', con=connection, schema='linkedin', if_exists='append',
+              dtype={
+                  "id": types.BigInteger(),
+                  "date": types.Date(),
+                  "title": types.Text(),
+                  "company_name": types.Text(),
+                  "company_link": types.Text(),
+                  "location": types.Text(),
+                  "job_link": types.Text(),
+                  "description": types.Text(),
+                  "seniority": types.Text(),
+                  "employment_type": types.Text(),
+                  "job_function": types.Text(),
+                  "industries": types.Text()
+              })
 
 
-# print(elems[0].text.strip())
 
 
-# # Load Chrome silently without popping up window
-# # options.add_argument('--headless')
-#
-# # Override system locale so that LinkedIn using English
-# options.add_argument("--lang=en-GB")
-#
-# # Fix window size large enough that LinkedIn will load job info on the same page instead of loading new one:
-# options.add_argument("window-size=1400,600")
-#
-# # Use proxy to load pages (in my case LinkedIn is blocked in Russia so I use Tor as local proxi):
-# options.add_argument('--proxy-server=socks5://' + proxy)
-#
-# # Prevent from console spam:
-# options.add_experimental_option('excludeSwitches', ['enable-logging'])
-#
-# # Make Chrome window stay after Python script finished working
-# options.add_experimental_option("detach", True)
-#
-# # Disable loading images to speed up things a bit:
-# prefs = {"profile.managed_default_content_settings.images": 2}
-# options.add_experimental_option("prefs", prefs)
-#
-#
-# wd = webdriver.Chrome(options=options, service=service)
-# # Load page
-# wd.get(url)
-#
-# # Test that we are not redirected to auth page
-# meta_path = ' / html / head / meta [contains(@name, "pageKey")]'
-# meta_content = wd.find_element(By.XPATH, meta_path).get_attribute('content')
-# if meta_content == 'auth_wall_desktop_jserp':
-#     wd.get(url)
-#
-#
-# ignored_exceptions = (NoSuchElementException, StaleElementReferenceException, TimeoutException)
-# elem = WebDriverWait(wd, timeout=10, ignored_exceptions=ignored_exceptions)
-#
-# # Close cookies and 'sign in' messages for easier debugging
-# try:
-#     accept_cookies_button = elem.until(
-#         expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, 'button.artdeco-global-alert-action'))).click()
-# except:
-#     pass
-#
-# try:
-#     close_signin_button = elem.until(
-#         expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, 'button.cta-modal__dismiss-btn'))).click()
-# except:
-#     pass
-#
-# # Get number of Job positions to parse
-# no_of_jobs_str = elem.until(
-#     expected_conditions.presence_of_element_located((By.CSS_SELECTOR, 'h1>span')))
-# no_of_jobs = int(no_of_jobs_str.text.replace(' ', '').replace('+', '').replace(',', ''))
-# print(str(no_of_jobs) + ' jobs found. Preprocessing titles list...')
-#
-# # -----------------------------------------------------------------------
-#
-# i = 2
-# while i <= int(no_of_jobs/25) + 1:
-#     wd.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-#     i = i + 1
-#     try:
-#         wd.find_element(By.XPATH, '/html/body/main/div/section/button').click()
-#         time.sleep(5)
-#     except:
-#         pass
-#         time.sleep(5)
-#
-# job_lists = wd.find_element(By.CSS_SELECTOR, '.jobs-search__results-list')
-#
-# jobs = job_lists.find_elements(By.CSS_SELECTOR, 'li')  # return a list
-#
-# # print('jobs' + jobs[0].text)
-# print(str(len(jobs)) + ' Processing base info...')
-#
-#
-# job_id = []
-# job_title = []
-# company_name = []
-# location = []
-# date = []
-# job_link = []
-# for job in jobs:
-#     job_id0 = None
-#     try:
-#         job_id0 = str(job.find_element(By.CSS_SELECTOR, '.base-card').get_attribute('data-entity-urn')).split(sep=':')[-1]
-#     except NoSuchElementException:
-#         pass
-#     job_id.append(job_id0)
-#
-#     job_title0 = None
-#     try:
-#         job_title0 = job.find_element(By.CSS_SELECTOR, '.base-search-card__info>h3').text
-#     except NoSuchElementException:
-#         pass
-#     job_title.append(job_title0)
-#
-#     company_name0 = None
-#     try:
-#         company_name0 = job.find_element(By.CSS_SELECTOR, '.base-search-card__info>h4').text
-#     except NoSuchElementException:
-#         pass
-#     company_name.append(company_name0)
-#
-#     location0 = None
-#     try:
-#         location0 = job.find_element(By.CSS_SELECTOR, '.job-search-card__location').text
-#     except NoSuchElementException:
-#         pass
-#     location.append(location0)
-#
-#     date0 = None
-#     try:
-#         date0 = job.find_element(By.CSS_SELECTOR, '[class*="job-search-card__listdate"]').get_attribute('datetime')
-#     except NoSuchElementException:
-#         pass
-#     date.append(date0)
-#
-#     job_link0 = None
-#     try:
-#         job_link0 = job.find_element(By.CSS_SELECTOR, 'a').get_attribute('href').split(sep='?')[0]
-#     except NoSuchElementException:
-#         pass
-#     job_link.append(job_link0)
-#
-#     print(f'{str(job_id0)}, {job_title0}, {company_name0}, {location0}, {date0}, {job_link0}')
-#
-#
-# print('Gathering more details...')
+
 #
 # jd =[]
 # seniority =[]
@@ -310,26 +242,9 @@ for idx, li in enumerate(lis):
 # # cleaning description column
 # # job_data['Description'] = job_data['Description'].str.replace('\n', ' ')
 # # job_data.to_excel('gis_jobs.xlsx', index=False)
-#
-# # Write data into Postgres
-# connection = create_engine(f'postgresql://{env_vars["user"]}:{env_vars["password"]}@{env_vars["host"]}:'
-#                            f'{env_vars["port"]}/{env_vars["dbname"]}')
-#
-# job_data.to_sql('open_positions', con=connection, schema='linkedin', if_exists='append',
-#               dtype={
-#                   "id": types.BigInteger(),
-#                   "date": types.Date(),
-#                   "company": types.Text(),
-#                   "title": types.Text(),
-#                   "location": types.Text(),
-#                   "description": types.Text(),
-#                   "level": types.Text(),
-#                   "type": types.Text(),
-#                   "function": types.ARRAY(types.Text),
-#                   "industry": types.ARRAY(types.Text),
-#                   "link": types.Text()
-#               })
-#
+
+
+
 #
 # # print(emp_type)
 # # print(seniority)
