f18446e66a499c46b9d0cf7a2b7661d374e9a606
Aleksandr Petrov <felispimeja@gmail.com>
2023-01-05 00:19:12 +0300

    Add redirect to login page check

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy import types
 
-
-
 # import environmental variables:
 env_vars = {}
 with open('./.env') as f:
@@ -25,7 +23,11 @@ with open('./.env') as f:
         if line.startswith('#') or not line.strip():
             continue
         key, value = line.strip().split('=', 1)
-        env_vars[key] = value # Save to a dict, initialized env_vars = {}
+        env_vars[key] = value  # Save to a dict, initialized env_vars = {}
+
+# PG connection:
+connection = create_engine(f'postgresql://{env_vars["user"]}:{env_vars["password"]}@{env_vars["host"]}:'
+                           f'{env_vars["port"]}/{env_vars["dbname"]}')
 
 #  Generate target urls:
 base_url = 'https://www.linkedin.com/jobs/api/seeMoreJobPostings/search'
@@ -38,10 +40,14 @@ url_list = [f'{base_url}?keywords={word}&location={location}&start='
 # https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=geo&location=Netherlands&start=600
 
 # for idx, url in enumerate(url_list): print(idx, url)
-url = url_list[14] #21
+url = url_list[14]  # 21
 
 proxy = 'socks5://localhost:9051'
-proxies = {"http": proxy, "https": proxy, "ftp": proxy }
+proxies = {"http": proxy, "https": proxy, "ftp": proxy}
+
+# ref_job_list = []
+# ref_job_list = pd.read_sql_query('select * from linkedin.open_positions', con=connection) #.id.values.tolist()
+# print(ref_job_list.to_string())
 
 job_list = []
 df = pd.DataFrame(columns=[
@@ -51,12 +57,13 @@ start = 0
 
 while start > -1:
     # print(url + str(start))
+
     page = requests.get(url=url + str(start), proxies=proxies)
     soup = bs4.BeautifulSoup(page.text, 'html.parser')
     if soup.find('li') is None:
         break
 
-    start = start+25
+    start = start + 25
     lis = soup.find_all('li')
 
     for idx, li in enumerate(lis):
@@ -77,15 +84,21 @@ while start > -1:
         job_link = job_link.split(sep='?')[0].replace('nl.linkedin.com', 'linkedin.com')
         # print(f'{idx+1: 3d}', job_id, date, job_title[0:30], company_name[0:10], location, job_link, company_link)
 
+        while True:
+            job_details = requests.get(url=job_link, proxies=proxies)
+            job_details = bs4.BeautifulSoup(job_details.text, 'html.parser')
+            if job_details.select_one('meta[name = "pageKey"]')['content'] == 'd_jobs_guest_details':
+                break
+
         job_details = requests.get(url=job_link, proxies=proxies)
         content = bs4.BeautifulSoup(job_details.text, 'html.parser').select_one('div.decorated-job-posting__details')
         description = content.select_one('div.description__text--rich').get_text('\n', strip=True)
-        further_details = content.select('li.description__job-criteria-item')
+        more_details = content.select('li.description__job-criteria-item')
         seniority = None
         employment_type = None
         job_function = None
         industries = None
-        for detail in further_details:
+        for detail in more_details:
             detail_type = detail.select_one('h3').get_text().strip()
             detail_detail = detail.select_one('span').get_text().strip()
             if detail_type == 'Seniority level':
@@ -96,156 +109,32 @@ while start > -1:
                 job_function = detail_detail.replace('and', '').replace(', ,', ',').split(sep=', ')
             elif detail_type == 'Industries':
                 industries = detail_detail.replace('and', '').replace(', ,', ',').split(sep=', ')
-        # print(seniority, employment_type, job_function, industries)
 
         info_string = (job_id, date, job_title, company_name, location, job_link, company_link,
                        description, seniority, employment_type, job_function, industries)
-        print(f'{idx+1: 3d}', info_string)
+        print(f'{idx + 1: 3d}', info_string)
         job_list.append(info_string)
         df = pd.concat([pd.DataFrame(
             [[job_id, date, job_title, company_name, company_link, location, job_link,
               description, seniority, employment_type, job_function, industries]], columns=df.columns
         ), df], ignore_index=True)
 
-# print(job_list)
 print(df.to_string())
 
 # Write data into Postgres
-connection = create_engine(f'postgresql://{env_vars["user"]}:{env_vars["password"]}@{env_vars["host"]}:'
-                           f'{env_vars["port"]}/{env_vars["dbname"]}')
-
+df = df.set_index('id')
 df.to_sql('open_positions', con=connection, schema='linkedin', if_exists='append',
-              dtype={
-                  "id": types.BigInteger(),
-                  "date": types.Date(),
-                  "title": types.Text(),
-                  "company_name": types.Text(),
-                  "company_link": types.Text(),
-                  "location": types.Text(),
-                  "job_link": types.Text(),
-                  "description": types.Text(),
-                  "seniority": types.Text(),
-                  "employment_type": types.Text(),
-                  "job_function": types.Text(),
-                  "industries": types.Text()
-              })
-
-
-
-
-
-#
-# jd =[]
-# seniority =[]
-# emp_type =[]
-# job_func =[]
-# industries =[]
-# for item in range(len(jobs)):
-#     job_func0 = []
-#     industries0 = []
-#
-#     job_click_path = f' // ul[contains(@class, "jobs-search__results-list")] / li[{item + 1}] // *'
-#     try:
-#         job.find_element(By.XPATH, job_click_path).click()
-#         time.sleep(1)
-#     except:
-#         print('Something went wrong (can not click job title to get job details)')
-#
-#     # clicking job to view job details
-#     # job_click_path = f' // ul[contains(@class, "jobs-search__results-list")] / li[{item + 1}] // *'
-#     # get_job_details = elem.until(
-#     #     expected_conditions.element_to_be_clickable((By.XPATH, job_click_path))).click()
-#
-#     # jd_path = ' //div[contains(@class, "description__text--rich")]'
-#     # jd0 = None
-#     # try:
-#     #     jd0 = job.find_element(By.XPATH, jd_path).get_attribute('innerText')
-#     # except NoSuchElementException:
-#     #     pass
-#     # jd.append(jd0)
-#
-#     # test_path = ' //h2[contains(@class, "top-card-layout__title")]'
-#     # test = job.find_element(By.XPATH, test_path)
-#     # print(test.text)
-#     # print(job_title[item])
-#
-#     a_path = ' //h2[contains(@class, "top-card-layout__title")]'
-#     a_check = elem.until(expected_conditions.text_to_be_present_in_element((By.XPATH, a_path), job_title[item]))
-#
-#     jd_path = ' //div[contains(@class, "description__text--rich")]'
-#     jd0 = elem.until(expected_conditions.presence_of_element_located((By.XPATH, jd_path))).get_attribute('innerText')
-#     jd.append(jd0)
-#
-#     seniority_path = ' // ul[contains(@class, "description__job-criteria-list")] / li[1] // span'
-#     seniority0 = None
-#     try:
-#         seniority0 = job.find_element(By.XPATH, seniority_path).get_attribute('innerText').strip()
-#     except NoSuchElementException:
-#         pass
-#
-#     seniority.append(seniority0)
-#
-#     emp_type_path = ' // ul[contains(@class, "description__job-criteria-list")] / li[2] // span'
-#     emp_type0 = None
-#     try:
-#         emp_type0 = job.find_element(By.XPATH, emp_type_path).get_attribute('innerText').strip()
-#     except NoSuchElementException:
-#         pass
-#
-#     emp_type.append(emp_type0)
-#
-#     job_func_path = ' // ul[contains(@class, "description__job-criteria-list")] / li[3] // span'
-#     job_func_elements_final = None
-#     try:
-#         job_func_elements = job.find_element(By.XPATH, job_func_path).get_attribute('innerText')
-#         job_func_elements_list = job_func_elements.replace('and ', ',').replace(', ,', ',').split(sep=',')
-#         job_func_elements_final = [s.strip() for s in job_func_elements_list]
-#     except NoSuchElementException:
-#         pass
-#
-#     job_func.append(job_func_elements_final)
-#
-#     industries_path = ' // ul[contains(@class, "description__job-criteria-list")] / li[4] // span'
-#     industries_elements_final = None
-#     try:
-#         industries_elements = job.find_element(By.XPATH, industries_path).get_attribute('innerText')
-#         industries_elements_list = industries_elements.replace('and ', ',').replace(', ,', ',').split(sep=',')
-#         industries_elements_final = [s.strip() for s in industries_elements_list]
-#     except NoSuchElementException:
-#         pass
-#
-#     industries.append(industries_elements_final)
-#
-#     # Check current page address:
-#     # get_url = wd.current_url
-#     # print("The current url is:" + str(get_url))
-#
-#     print(f'{item + 1} {jd0[:30]}..., {seniority0}, {emp_type0}, {job_func_elements_final}, {industries_elements_final}')
-#
-# # print(len(job_id), len(date), len(company_name), len(job_title), len(location), len(jd), len(seniority), len(emp_type), len(job_func), len(industries), len(job_link) )
-#
-#
-# job_data = pd.DataFrame({
-#     'id': job_id,
-#     'date': date,
-#     'company': company_name,
-#     'title': job_title,
-#     'location': location,
-#     'description': jd,
-#     'level': seniority,
-#     'type': emp_type,
-#     'function': job_func,
-#     'industry': industries,
-#     'link': job_link
-# })
-#
-# # cleaning description column
-# # job_data['Description'] = job_data['Description'].str.replace('\n', ' ')
-# # job_data.to_excel('gis_jobs.xlsx', index=False)
-
-
-
-#
-# # print(emp_type)
-# # print(seniority)
-# # print(job_func)
+          dtype={
+              "id": types.BigInteger(),
+              "date": types.Date(),
+              "title": types.Text(),
+              "company_name": types.Text(),
+              "company_link": types.Text(),
+              "location": types.Text(),
+              "job_link": types.Text(),
+              "description": types.Text(),
+              "seniority": types.Text(),
+              "employment_type": types.Text(),
+              "job_function": types.Text(),
+              "industries": types.Text()
+          })
